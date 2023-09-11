f02091d3f342fe1cc871d107d51bc55238fce64b
adetimmy <ogunleademola@gmail.com>
2023-09-11 08:15:43 +0100

    mobile tab adjustment2

--- components/mobileMenu.tsx
+++ components/mobileMenu.tsx
@@ -10,11 +10,11 @@ export const MobileMenu = (menu:any) => {
                 <Image width={90} src={Ade} alt ='user-picture' className='rounded-full aspect-square bg-cover '/>
             </div>
             <div className='flex flex-col items-center justify-between  mt-4 gap-3 text-[#667085]'>
-                <div className='flex justify-start gap-3 items-center cursor-pointer w-full text-base  dark:bg-gray-300 hover:bg-blue-600 hover:text-white px-3 py-2'>
+                <div className='flex justify-start gap-1 items-center cursor-pointer w-full text-sm  dark:bg-gray-300 hover:bg-blue-600 hover:text-white px-3 py-2'>
                 <AiOutlineSetting size={25}/>
                 <p>Settings</p>
                 </div>
-                <div className='flex justify-start gap-3 items-center mb-2 cursor-pointer w-full text-base  dark:bg-gray-300 hover:bg-blue-600 hover:text-white px-3 py-2'>
+                <div className='flex justify-start gap-1 items-center mb-2 cursor-pointer w-full text-sm  dark:bg-gray-300 hover:bg-blue-600 hover:text-white px-3 py-2'>
                 <AiOutlineBell size={25}/>
                 <p>Notifications</p>
                 </div> 
